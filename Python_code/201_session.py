6f077eafb72646d151030b6d93bd5e08a704454a
WangJingfang <373264325@qq.com>
2018-03-21 09:47:33 +0800

    Update 201_session.py

--- Python_code/201_session.py
+++ Python_code/201_session.py
@@ -1,1 +1,25 @@
+"""
+Dependencies:
+tensorflow:1.1.6
+"""
 
+import tensorflow as tf
+
+m1 = tf.constant([[2, 2]])
+m2 = tf.constant([[3],
+                  [3]])
+dot_operation = tf.matmul(m1,m2)
+
+print(dot_operation)  # wrong! no result
+
+# launch the graph in a session
+# method1 use session
+sess = tf.Session()
+result = sess.run(dot_operation)
+print(result)  # Evaluate the tensor 'c'.
+sess.close()
+
+# method2 use session
+with tf.Session() as sess:
+    result_ = sess.run(dot_operation)
+    print(result_)
